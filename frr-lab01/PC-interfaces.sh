6920234da43cd65e291f42c482b855866b1c4a6d
Ronny Trommer <ronny@no42.org>
2024-01-19 00:13:24 +0100

    chg: Renumbering with ptp /31 networks and enabled IS-IS on router4-router-6

--- frr-lab01/PC-interfaces.sh
+++ frr-lab01/PC-interfaces.sh
@@ -1,30 +1,24 @@
 #!/bin/sh
 sudo docker exec -d clab-frr01-pc1 ip link set eth1 up
-sudo docker exec -d clab-frr01-pc1 ip addr add 192.168.11.2/24 dev eth1
+sudo docker exec -d clab-frr01-pc1 ip addr add 192.168.1.11/24 dev eth1
 sudo docker exec -d clab-frr01-pc1 ip route add 192.168.0.0/16 via 192.168.11.1 dev eth1
-sudo docker exec -d clab-frr01-pc1 ip route add 10.10.10.0/24 via 192.168.11.1 dev eth1
 
 sudo docker exec -d clab-frr01-pc2 ip link set eth1 up
-sudo docker exec -d clab-frr01-pc2 ip addr add 192.168.12.2/24 dev eth1
+sudo docker exec -d clab-frr01-pc2 ip addr add 192.168.2.11/24 dev eth1
 sudo docker exec -d clab-frr01-pc2 ip route add 192.168.0.0/16 via 192.168.12.1 dev eth1
-sudo docker exec -d clab-frr01-pc2 ip route add 10.10.10.0/24 via 192.168.12.1 dev eth1
 
 sudo docker exec -d clab-frr01-pc3 ip link set eth1 up
-sudo docker exec -d clab-frr01-pc3 ip addr add 192.168.13.2/24 dev eth1
+sudo docker exec -d clab-frr01-pc3 ip addr add 192.168.3.11/24 dev eth1
 sudo docker exec -d clab-frr01-pc3 ip route add 192.168.0.0/16 via 192.168.13.1 dev eth1
-sudo docker exec -d clab-frr01-pc3 ip route add 10.10.10.0/24 via 192.168.13.1 dev eth1
 
 sudo docker exec -d clab-frr01-pc4 ip link set eth1 up
-sudo docker exec -d clab-frr01-pc4 ip addr add 192.168.14.2/24 dev eth1
+sudo docker exec -d clab-frr01-pc4 ip addr add 192.168.4.11/24 dev eth1
 sudo docker exec -d clab-frr01-pc4 ip route add 192.168.0.0/16 via 192.168.14.1 dev eth1
-sudo docker exec -d clab-frr01-pc4 ip route add 10.10.10.0/24 via 192.168.14.1 dev eth1
 
 sudo docker exec -d clab-frr01-pc5 ip link set eth1 up
-sudo docker exec -d clab-frr01-pc5 ip addr add 192.168.15.2/24 dev eth1
+sudo docker exec -d clab-frr01-pc5 ip addr add 192.168.5.11/24 dev eth1
 sudo docker exec -d clab-frr01-pc5 ip route add 192.168.0.0/16 via 192.168.15.1 dev eth1
-sudo docker exec -d clab-frr01-pc5 ip route add 10.10.10.0/24 via 192.168.15.1 dev eth1
 
 sudo docker exec -d clab-frr01-pc6 ip link set eth1 up
-sudo docker exec -d clab-frr01-pc6 ip addr add 192.168.16.2/24 dev eth1
+sudo docker exec -d clab-frr01-pc6 ip addr add 192.168.6.11/24 dev eth1
 sudo docker exec -d clab-frr01-pc6 ip route add 192.168.0.0/16 via 192.168.16.1 dev eth1
-sudo docker exec -d clab-frr01-pc6 ip route add 10.10.10.0/24 via 192.168.16.1 dev eth1
