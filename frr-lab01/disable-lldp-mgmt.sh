a267e3d4eaff1a52f1e72df51adb0b1b5a3ca32d
Ronny Trommer <ronny@no42.org>
2024-01-20 02:18:47 +0100

    feat: Adding eBGP routing
    Created a router6 in AS65501 and created a new router7 in AS65502.
    Configured minimal BGP settings and basic IP address configuration.

--- frr-lab01/disable-lldp-mgmt.sh
+++ frr-lab01/disable-lldp-mgmt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for i in {1..5}; do
+for i in {1..7}; do
   echo "Disabled LLDP on eth0 on clab-frr01-router${i}."
   docker exec clab-frr01-router${i} lldpcli configure system interface pattern \!eth0,eth*;
 done
