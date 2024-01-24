4ef8ad90b180dde0fae719731b62a934519fa7c4
Ronny Trommer <ronny@no42.org>
2024-01-24 14:52:33 +0100

    feat: Add OSPF routing lab troublshooting SNMP
    Build a 3 router network topology with simple OSPF topology using Arista cEOS images.

---
+++ ceos-lab01/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Maybe it's time for Batfish :)
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+checkError() {
+  if [[ "${1}" -eq 0 ]]; then
+    echo -e "${GREEN} ✔ ${ENDCOLOR}"
+  else
+    echo -e "${RED} ✖ ${ENDCOLOR}"
+  fi
+}
+
+echo ""
+echo "### Check reachability from PC1 (192.168.1.11/24)"
+echo ""
+echo -n " - Test gateway 192.168.1.1:                   "
+docker exec -it clab-ceos01-pc1 ping -W 0.5 -c 1 192.168.1.1 2>&1 > /dev/null
+checkError "${?}"
+echo -n " - Test connectivity to PC2 (192.168.2.11/24): "
+docker exec -it clab-ceos01-pc1 ping -W 0.5 -c 1 192.168.2.11 2>&1 > /dev/null
+checkError "${?}"
+echo -n " - Test connectivity to PC3 (192.168.3.11/24): "
+docker exec -it clab-ceos01-pc1 ping -W 0.5 -c 1 192.168.3.11 2>&1 > /dev/null
+checkError "${?}"
---
+++ frr-lab02/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+clab deploy --topo frr02.clab.yml
+./pc-interfaces.sh
